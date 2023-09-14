1cab681972082f2a5f93f2dd5ba0db9bdeb34a61
Manon <manonlefkos@gmail.com>
2023-09-14 13:36:46 +0200

    remove functional Input component from project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import ClassInput from "./ClassInput";
-import FunctionalInput from "./FunctionalInput";
 
 function App() {
   return (
     <div>
-      <FunctionalInput name="Functional Input" />
       <ClassInput name="Class Input" />
     </div>
   );
